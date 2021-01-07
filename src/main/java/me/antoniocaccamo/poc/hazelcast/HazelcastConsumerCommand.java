88ae06bb483abc784ab4e5958a77de861479ad77
antonio caccamo <caccamo.antonio@gmail.com>
2021-01-07 13:10:14 +0100

    RFC : callable

--- src/main/java/me/antoniocaccamo/poc/hazelcast/HazelcastConsumerCommand.java
+++ src/main/java/me/antoniocaccamo/poc/hazelcast/HazelcastConsumerCommand.java
@@ -5,39 +5,36 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.topic.ITopic;
 import com.hazelcast.topic.Message;
 import com.hazelcast.topic.MessageListener;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine.Command;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
 
 @Slf4j
 @Command(name = "consumer") 
-public class HazelcastConsumerCommand implements Runnable{
+public class HazelcastConsumerCommand implements Callable<Void> {
 
-
-
-    @SneakyThrows
-    public void run() {
+    @Override
+    public Void call() throws Exception {
         // business logic here
-        
+
         log.info("Hi from consumer!");
 
-        Runtime.getRuntime()
-                .addShutdownHook( new Thread(() -> HazelcastClient.shutdownAll()));
-        
+        Runtime.getRuntime().addShutdownHook( new Thread(() -> {
+                log.info("shutting down consumer");
+                HazelcastClient.shutdownAll();
+            })
+        );
+
         HazelcastInstance client = HazelcastClient.newHazelcastClient();
         ITopic topic = client.getTopic(HazelcastCommand.Constants.Topic);
         topic.addMessageListener( new HazelcastCommandMessageListener() );
 
-        for (; ; ) {
-            Thread.sleep(1000);
-        }
-
+        return null;
     }
-    
-    private static class HazelcastCommandMessageListener implements MessageListener<LocalDateTime> {
 
+    private static class HazelcastCommandMessageListener implements MessageListener<LocalDateTime> {
 
         @Override
         public void onMessage(Message<LocalDateTime> message) {
--- src/main/java/me/antoniocaccamo/poc/hazelcast/HazelcastProducerCommand.java
+++ src/main/java/me/antoniocaccamo/poc/hazelcast/HazelcastProducerCommand.java
@@ -1,55 +1,41 @@
 package me.antoniocaccamo.poc.hazelcast;
 
-import com.hazelcast.topic.ITopic;
-import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
-
 import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
-
-import lombok.SneakyThrows;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
+import com.hazelcast.topic.ITopic;
 import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine.Command;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static picocli.CommandLine.Command;
-
 @Slf4j
 @Command(name = "producer") 
-public class HazelcastProducerCommand implements Runnable {
+public class HazelcastProducerCommand implements Callable<Void> {
 
-    @SneakyThrows
-    public void run() {
+    @Override
+    public Void call() throws Exception {
         // business logic here
         log.info("Hi from producer!");
-        Runtime.getRuntime()
-                .addShutdownHook( new Thread(() -> HazelcastClient.shutdownAll()));
+        Runtime.getRuntime().addShutdownHook( new Thread(() -> {
+                    log.info("shutting down producer");
+                    HazelcastClient.shutdownAll();
+                })
+        );
         HazelcastInstance client = HazelcastClient.newHazelcastClient();
         ITopic topic = client.getTopic(HazelcastCommand.Constants.Topic);
         Executors.newSingleThreadScheduledExecutor()
-
-                .scheduleWithFixedDelay(() -> {
+            .scheduleWithFixedDelay(() -> {
                         log.info("publishing..");
                         topic.publish(LocalDateTime.now());
                     },
+                    1000 ,
+                    400,
+                    TimeUnit.MILLISECONDS
+            );
 
-                        1000 ,
-                        400,
-                TimeUnit.MILLISECONDS
-                );
-
-
-        for (; ; ) {
-            Thread.sleep(1000);
-        }
+        return null;
     }
-    
 }
